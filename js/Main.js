934714648a07d39aa2c9fe5fdc03fdd2b64ea559
Hu Chen <hu.chen@honestbee.com>
2016-07-13 12:18:49 +0800

    change Main nav to ListView

--- js/Main.js
+++ js/Main.js
@@ -4,38 +4,82 @@ import {
   TouchableHighlight,
   Text,
   View,
+  ListView,
 } from 'react-native'
 import Day1 from './Day1';
+import * as colors from './colors';
 
 // TODO: run on iOS device and debug
-// TODO: change Main to list view
 
 export default class Main extends Component {
-  goToDay1 = () => {
-    this.props.navigator.push({
-      title: 'Stopwatch',
-      component: Day1
-    });
+  ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+  state = {
+    dataSource: this.ds.cloneWithRows([
+      {
+        day: 1,
+        title: "Stopwatch"
+      }
+    ])
+  }
+
+  goToDay = (day) => {
+    if (day === 1) {
+      this.props.navigator.push({
+        title: 'Stopwatch',
+        component: Day1
+      });
+    }
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableHighlight
-          onPress={this.goToDay1}
-          underlayColor="white">
-          <Text>Day 1</Text>
-        </TouchableHighlight>
-      </View>
+      <ListView
+        style={{
+          flex: 1
+        }}
+        dataSource={this.state.dataSource}
+        enableEmptySections
+        renderRow={(rowData, sectionID, rowID) => (
+          <TouchableHighlight
+            onPress={this.goToDay.bind(null, rowData.day)}
+            underlayColor={colors.tableUnderlay}
+            style={{
+              paddingLeft: 15,
+              paddingRight: 15,
+              height: 44,
+              justifyContent: 'center',
+            }}>
+            <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+              <Text style={{
+                  color: colors.textSecondary,
+                  fontSize: 17
+                }}>Day {rowData.day}</Text>
+              <Text style={{
+                  color: colors.textPrimary,
+                  fontSize: 17,
+                }}>{rowData.title}</Text>
+            </View>
+          </TouchableHighlight>
+        )}
+        renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => (
+          <View
+            key={`${sectionID}-${rowID}`}
+            style={{
+              marginLeft: 15,
+              height: StyleSheet.hairlineWidth,
+              backgroundColor: colors.separator,
+            }}
+          />
+        )}
+      />
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 64,
-    alignItems: "center",
-    justifyContent: "center",
-  }
 });
--- js/colors.js
+++ js/colors.js
@@ -4,3 +4,4 @@ export const separator = '#C8C7CC';
 export const textSecondary = '#8F8F94';
 export const textPrimary = '#000';
 export const buttonUnderlay = '#888';
+export const tableUnderlay = '#C9C9CE';
