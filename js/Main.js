8440d3a039a5c2efb2c918ccb624325d0b81f671
Hu Chen <chen@huchen.me>
2016-07-19 23:59:33 +0800

    main nav styles

--- js/Main.js
+++ js/Main.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  TouchableHighlight,
   Text,
   View,
   StatusBar,
-  ListView,
   DeviceEventEmitter,
+  TouchableHighlight,
+  ScrollView,
+  Image
 } from 'react-native'
-import Separator from './Separator';
 import * as colors from './colors';
 import days from './days';
-// TODO: nav styles
-// TODO: app icon
+import {screenWidth, screenHeight} from './dimensions';
+import Icon from 'react-native-vector-icons/Ionicons';
+import IconFA from 'react-native-vector-icons/FontAwesome';
+import Swiper from 'react-native-swiper';
+
+const DayBox = ({day, index, goToDay}) => (
+  <TouchableHighlight
+    style={[styles.touchBox, index%3 == 2 && styles.touchBoxRight]}
+    underlayColor="#eee"
+    onPress={goToDay}>
+    <View style={styles.boxContainer}>
+      <Text style={styles.boxText}>Day {day.day}</Text>
+      {day.isFA
+        ? <IconFA size={day.size} name={day.icon} style={[styles.boxIcon,{color:day.color}]}></IconFA>
+        : <Icon size={day.size} name={day.icon} style={[styles.boxIcon,{color:day.color}]}></Icon>
+      }
+    </View>
+  </TouchableHighlight>
+)
 
 export default class Main extends Component {
-  ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-
-  state = {
-    dataSource: this.ds.cloneWithRows(days)
-  }
-
   componentWillMount() {
     StatusBar.setBarStyle('default');
     DeviceEventEmitter.addListener(
@@ -62,33 +73,42 @@ export default class Main extends Component {
 
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        enableEmptySections
-        renderRow={(rowData, sectionID, rowID, highlightRow) => (
-          <TouchableHighlight
-            underlayColor={colors.tableUnderlay}
-            onPress={() => {
-              this.goToDay(rowID);
-              highlightRow(sectionID, rowID);
-            }}
-            style={styles.rowContainer}>
-            <View style={styles.row}>
-              <Text style={{
-                  color: colors.textSecondary,
-                  fontSize: 17
-                }}>Day {rowData.day}</Text>
-              <Text style={{
-                  color: colors.textPrimary,
-                  fontSize: 17,
-                }}>{rowData.title}</Text>
+      <ScrollView>
+        <Swiper
+          height={150}
+          showsButtons={false}
+          autoplay={true}
+          activeDot={<View style={styles.activeDot} />}>
+          <TouchableHighlight onPress={() => this.goToDay(1)}>
+            <View style={styles.slide}>
+              <Image
+                resizeMode="cover"
+                style={styles.image}
+                source={require('./assets/banner1.png')} />
+              <Text style={styles.slideText}>Day {days[1].day}: {days[1].title}</Text>
             </View>
           </TouchableHighlight>
-        )}
-        renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => (
-          <Separator key={`${sectionID}-${rowID}`} />
-        )}
-      />
+          <TouchableHighlight onPress={() => this.goToDay(2)}>
+            <View style={styles.slide}>
+              <Image
+                resizeMode="cover"
+                style={styles.image}
+                source={require('./assets/banner2.png')} />
+              <Text style={styles.slideText}>Day {days[2].day}: {days[2].title}</Text>
+            </View>
+          </TouchableHighlight>
+        </Swiper>
+        <View style={styles.touchBoxContainer}>
+          {days.map((day, index) => (
+            <DayBox
+              key={index}
+              day={day}
+              index={index}
+              goToDay={()=>{this.goToDay(index)}}
+            />
+          ))}
+        </View>
+      </ScrollView>
     )
   }
 }
@@ -104,5 +124,73 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  touchBoxContainer:{
+    flexDirection: "row",
+    flexWrap:"wrap",
+    width: screenWidth,
+    alignItems: 'flex-start',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor:"#ccc",
+    borderBottomWidth: 0
+  },
+  touchBox: {
+    backgroundColor:"#fff",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderRightWidth: StyleSheet.hairlineWidth,
+    borderBottomColor:"#ccc",
+    borderRightColor:"#ccc",
+    width: screenWidth/3 - StyleSheet.hairlineWidth,
+    height: screenWidth/3 - StyleSheet.hairlineWidth,
+  },
+  touchBoxRight: {
+    borderRightWidth: 0,
+  },
+  boxContainer: {
+    alignItems:"center",
+    justifyContent:"center",
+    flex: 1
+  },
+  boxText: {
+    position: "absolute",
+    bottom: 15,
+    width: screenWidth/3,
+    textAlign: "center",
+    left: 0,
+    backgroundColor: "transparent"
+  },
+  boxIcon:{
+    position: "relative",
+    top:-10
+  },
+  slide: {
+    flex: 1,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slideText:{
+    position:"absolute",
+    bottom: 0,
+    paddingTop:5,
+    paddingBottom:5,
+    backgroundColor:"rgba(255,255,255,0.5)",
+    width: screenWidth,
+    textAlign:"center",
+    fontSize: 12
+  },
+  image:{
+    width: screenWidth,
+    height: 150
+  },
+  activeDot: {
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginLeft: 3,
+    marginRight: 3,
+    marginTop: 3,
+    marginBottom: 3,
   }
 });
--- js/days.js
+++ js/days.js
@@ -18,84 +18,140 @@ const days = [
     day: 1,
     title: "Stopwatch",
     component: Day1,
-    hideNav: false
+    hideNav: false,
+    isFA: false,
+    icon: "ios-stopwatch",
+    size: 48,
+    color: "#ff856c",
   },
   {
     day: 3,
     title: "Twitter",
     component: Day3,
+    isFA: false,
+    icon: "logo-twitter",
+    size:50,
+    color:"#2aa2ef",
     hideNav: true
   },
   {
     day: 4,
     title: "Cocoapods",
     component: Day4,
+    isFA: true,
+    icon: "contao",
+    size:50,
+    color:"#FF9A05",
     hideNav: false
   },
   {
     day: 5,
     title: "Find my location",
     component: Day5,
+    isFA: false,
+    icon: "ios-pin",
+    size:50,
+    color:"#00D204",
     hideNav: false
   },
   {
     day: 6,
     title: "Spotify",
     component: Day6,
+    isFA: true,
+    icon: "spotify",
+    size:50,
+    color:"#777",
     hideNav: true
   },
   {
     day: 8,
     title: "Swipe Menu",
     component: Day8,
+    isFA: true,
+    icon: "google",
+    size:50,
+    color:"#4285f4",
     hideNav: true
   },
   {
     day: 9,
     title: "Twitter Profile",
     component: Day9,
+    isFA: false,
+    icon: "ios-egg",
+    size:50,
+    color:"#2aa2ef",
     hideNav: true
   },
   {
     day: 17,
     title: "Fuzzy search",
     component: Day17,
+    isFA: false,
+    icon: "ios-search",
+    size:50,
+    color:"#69B32A",
     hideNav: false
   },
   {
     day: 19,
     title: "Touch ID",
     component: Day19,
+    isFA: false,
+    icon: "ios-log-in",
+    size:50,
+    color:"#fdbded",
     hideNav: false
   },
   {
     day: 20,
     title: "Reminders",
     component: Day20,
+    isFA: false,
+    icon: "ios-list-outline",
+    size:50,
+    color:"#68d746",
     hideNav: true
   },
   {
     day: 21,
     title: "Multi Reminders",
     component: Day21,
+    isFA: false,
+    icon: "ios-paper-outline",
+    size:50,
+    color:"#fe952b",
     hideNav: true
   },
   {
     day: 24,
     title: "Youtube",
     component: Day24,
+    isFA: false,
+    icon: "logo-youtube",
+    size:50,
+    color:"#e32524",
     hideNav: true
   },
   {
     day: 25,
     title: "WebView",
     component: Day25,
+    isFA: false,
+    icon: "ios-compass",
+    size:50,
+    color:"#00ab6b",
     hideNav: true
   },
   {
     day: 29,
     title: "3D Touch",
     component: Day29,
+    isFA: false,
+    icon: "md-menu",
+    size:50,
+    color:"#48f52e",
     hideNav: false
   },
 ];
