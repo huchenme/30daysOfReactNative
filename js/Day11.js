de9092401f86ed0cddffd461fa38582e1b455068
Hu Chen <chen@huchen.me>
2016-07-27 00:14:10 +0800

    re style menu of day 26

--- js/Day11.js
+++ js/Day11.js
@@ -20,50 +20,49 @@ Header.propTypes = {
 const shaders = GL.Shaders.create({
   helloGL: {
     frag: `
-      precision highp float
-      varying vec2 uv
-      uniform float value
+      precision highp float;
+      varying vec2 uv;
+      uniform float value;
       void main () {
-        gl_FragColor = vec4(uv.x, uv.y, value, 1.0)
+        gl_FragColor = vec4(uv.x, uv.y, value, 1.0);
       }
     `,
   },
   saturation: {
     frag: `
-      precision highp float
-      varying vec2 uv
-      uniform sampler2D image
-      uniform float factor
+      precision highp float;
+      varying vec2 uv;
+      uniform sampler2D image;
+      uniform float factor;
       void main () {
-        vec4 c = texture2D(image, uv)
-        // Algorithm from Chapter 16 of OpenGL Shading Language
-        const vec3 W = vec3(0.2125, 0.7154, 0.0721)
-        gl_FragColor = vec4(mix(vec3(dot(c.rgb, W)), c.rgb, factor), c.a)
+        vec4 c = texture2D(image, uv);
+        const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+        gl_FragColor = vec4(mix(vec3(dot(c.rgb, W)), c.rgb, factor), c.a);
       }
     `,
   },
   pieProgress: {
     frag: `
-      precision mediump float
-      varying vec2 uv
-      uniform vec4 colorInside, colorOutside
-      uniform float radius
-      uniform float progress
-      uniform vec2 dim
-      const vec2 center = vec2(0.5)
-      const float PI = acos(-1.0)
+      precision mediump float;
+      varying vec2 uv;
+      uniform vec4 colorInside, colorOutside;
+      uniform float radius;
+      uniform float progress;
+      uniform vec2 dim;
+      const vec2 center = vec2(0.5);
+      const float PI = acos(-1.0);
       void main () {
-      vec2 norm = dim / min(dim.x, dim.y)
-      vec2 p = uv * norm - (norm-1.0)/2.0
-      vec2 delta = p - center
-      float inside =
-        step(length(delta), radius) *
-        step((PI + atan(delta.y, - 1.0 * delta.x)) / (2.0 * PI), progress)
-      gl_FragColor = mix(
-        colorOutside,
-        colorInside,
-        inside
-      )
+        vec2 norm = dim / min(dim.x, dim.y);
+        vec2 p = uv * norm - (norm-1.0)/2.0;
+        vec2 delta = p - center;
+        float inside =
+          step(length(delta), radius) *
+          step((PI + atan(delta.y, - 1.0 * delta.x)) / (2.0 * PI), progress);
+        gl_FragColor = mix(
+          colorOutside,
+          colorInside,
+          inside
+        );
       }
     `,
   },
--- js/Day26.js
+++ js/Day26.js
@@ -1,15 +1,142 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import * as dimensions from './dimensions'
 import {
-  View,
-  Text,
+  Animated,
   StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  View,
+  PanResponder,
+  LayoutAnimation,
 } from 'react-native'
+import Day22 from './Day22'
+import Day24 from './Day24'
+import Day25 from './Day25'
+
+const menuWidth = 150
+const minLeft = -menuWidth - 10
+const customLayoutLinear = {
+  duration: 200,
+  create: {
+    type: LayoutAnimation.Types.linear,
+    property: LayoutAnimation.Properties.left,
+  },
+  update: {
+    type: LayoutAnimation.Types.easeInEaseOut,
+  },
+}
+
+const Row = ({children}) => (
+  <TouchableHighlight
+    underlayColor='rgba(255,255,255,0.15)'
+    style={styles.row}
+    onPress={this.props.onPress}>
+    <Text style={styles.text}>
+      {this.props.children}
+    </Text>
+  </TouchableHighlight>
+)
+Row.propTypes = {
+  children: PropTypes.any.isRequired,
+  onPress: PropTypes.func,
+}
+
+const Menu = () => (
+  <View style={styles.menu}>
+    <Row onPress={()=>{console.log('day 22')}}>Day 22</Row>
+    <Row onPress={()=>{console.log('day 24')}}>Day 24</Row>
+    <Row onPress={()=>{console.log('day 25')}}>Day 25</Row>
+  </View>
+)
 
 export default class Day26 extends Component {
+  state = {
+    showDrop: false,
+    dropOpacity: 0,
+    menuLeft: minLeft,
+  }
+
+  previousMenuLeft = minLeft
+  previousDropOpacity = 0
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        this.setState({showDrop: true})
+      },
+      onPanResponderMove: this.onPanResponderMove,
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: this.onPanResponderEnd,
+      onPanResponderTerminate: this.onPanResponderEnd,
+      onShouldBlockNativeResponder: (evt, gestureState) => true,
+    })
+  }
+
+  onPanResponderMove = (evt, gestureState) => {
+    let newMenuLeft = this.previousMenuLeft + gestureState.dx
+    let newDropOpacity = this.previousDropOpacity + Math.pow((gestureState.dx / -minLeft), 0.5)
+    if (newMenuLeft > 0) {
+      newMenuLeft = 0
+      newDropOpacity = 1
+    }
+    if (newMenuLeft < minLeft) {
+      newMenuLeft = minLeft
+      newDropOpacity = 0
+    }
+    this.setState({
+      menuLeft: newMenuLeft,
+      dropOpacity: newDropOpacity,
+    })
+  }
+
+  closeMenu = () => {
+    this.previousMenuLeft = minLeft
+    this.newDropOpacity = 0
+    this.setState({
+      dropOpacity: 0,
+      menuLeft: minLeft,
+      showDrop: false,
+    })
+    LayoutAnimation.configureNext(customLayoutLinear)
+  }
+
+  openMenu = () => {
+    this.previousMenuLeft = 0
+    this.newDropOpacity = 1
+    this.setState({
+      dropOpacity: 1,
+      menuLeft: 0,
+    })
+    LayoutAnimation.configureNext(customLayoutLinear)
+  }
+
+  onPanResponderEnd = (evt, gestureState) => {
+    if (gestureState.vx < 0 || gestureState.dx < 0) {
+      this.closeMenu()
+    } else if (gestureState.vx > 0 || gestureState.dx > 0) {
+      this.openMenu()
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Day 26</Text>
+        <View style={styles.menuContainer}>
+          <Menu />
+        </View>
+        <Animated.View style={styles.container} {...this._panResponder.panHandlers}>
+          <Day22 />
+        </Animated.View>
+        {false && this.state.showDrop && (
+          <TouchableWithoutFeedback onPress={() => {this.closeMenu()}}>
+            <View style={[styles.drop, {opacity: this.state.dropOpacity}]} />
+          </TouchableWithoutFeedback>
+        )}
       </View>
     )
   }
@@ -18,8 +145,34 @@ export default class Day26 extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 64,
-    alignItems: 'center',
+  },
+  drop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  row: {
+    height: 48,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#fff',
+  },
+  text: {
+    color: 'white',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  menuContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    width: menuWidth + 20,
+  },
+  menu: {
+    backgroundColor: '#893D54',
+    width: menuWidth,
+    height: dimensions.screenHeight,
     justifyContent: 'center',
   },
 })
