5dc59cea1d929f40a227b4711c344c42ed38c6cb
Hu Chen <hu.chen@honestbee.com>
2016-08-02 11:13:50 +0800

    copy styles from demo

--- js/Day2.js
+++ js/Day2.js
@@ -1,36 +1,417 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {
-  View,
-  Text,
+  Image,
+  ScrollView,
+  StatusBar,
   StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
 } from 'react-native'
+import weatherData from './weatherData'
+import {screenWidth, screenHeight} from './dimensions'
+import Icon from 'react-native-vector-icons/Ionicons'
 import Swiper from 'react-native-swiper'
 
-export default class Day2 extends Component {
+class Weather extends Component{
+  static propTypes = {
+    back: React.PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      weather: weatherData,
+    }
+  }
+
+  _back() {
+    this.props.back()
+  }
+
   render() {
-    return (
-      <Swiper style={styles.wrapper}>
-        <View style={styles.container}>
-          <Text>Day 1</Text>
+    const slides = this.state.weather.map((elem, index) => {
+      const hourView = elem.hours.map((hourElem, hourIndex) => {
+        return (
+          <View key={hourElem.key} style={styles.withinDayHoursBox}>
+            <Text style={hourIndex == 0 ? styles.withinDayHoursTimeBold : styles.withinDayHoursTime}>{hourElem.time}</Text>
+            <Icon name={hourElem.icon} size={25} style={[styles.withinDayHoursIcon, {color: hourElem.color}]} />
+            <Text style={hourIndex == 0 ? styles.withinDayHoursDegreeBold : styles.withinDayHoursDegree}>{hourElem.degree}</Text>
+          </View>
+        )
+      })
+
+      const dayView = elem.days.map((dayElem, dayIndex) => {
+        return (
+          <View key={dayElem.key} style={styles.withinWeekLine}>
+            <View style={styles.withinWeekDay}>
+              <Text style={styles.withinWeekDayText}>{dayElem.day}</Text>
+            </View>
+            <View style={styles.withinWeekIcon}>
+              <Icon name={dayElem.icon} style={styles.withinWeekIconIcon} size={25} />
+            </View>
+            <View style={styles.withinWeekDegree}>
+              <Text style={styles.withinWeekHigh}>{dayElem.high}</Text>
+              <Text style={elem.night ? styles.withinWeekLowNight : styles.withinWeekLow}>{dayElem.low}</Text>
+            </View>
+          </View>
+        )
+      })
+
+      return (
+        <View key={elem.key}>
+          <Image style={styles.image} source={elem.bg} />
+          <ScrollView style={styles.pageContainer}  showsVerticalScrollIndicator={false}>
+            <View style={styles.headInfo}>
+              <Text style={styles.city}>{elem.city}</Text>
+              <Text style={styles.abs}>{elem.abs}</Text>
+              <Text style={styles.degree}>{elem.degree}</Text>
+              <Text style={styles.circle}>°</Text>
+            </View>
+            <View style={styles.withinDay}>
+              <View style={styles.withinDayGeneral}>
+                <View style={styles.withinDayHead}>
+                  <Text style={styles.withinDayWeek}>{elem.today.week}</Text>
+                  <Text style={styles.withinDayDay}>{elem.today.day}</Text>
+                </View>
+                <View style={styles.withinDayTail}>
+                  <Text style={styles.withinDayHigh}>{elem.today.high}</Text>
+                  <Text style={elem.night ? styles.withinDayLowNight : styles.withinDayLow}>{elem.today.low}</Text>
+                </View>
+              </View>
+              <ScrollView  horizontal={true} showsHorizontalScrollIndicator={false} style={styles.withinDayHoursContainer}>
+                <View style={styles.withinDayHours}>
+                  {hourView}
+                </View>
+              </ScrollView>
+              <View style={styles.withinWeek}>
+                {dayView}
+              </View>
+              <View style={styles.weatherInfo}>
+                <Text style={styles.weatherInfoText}>{elem.info}</Text>
+              </View>
+              <View style={styles.weatherOther}>
+                <View style={styles.weatherOtherSection}>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>日出：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.rise}</Text>
+                  </View>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>日落：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.down}</Text>
+                  </View>
+                </View>
+                <View style={styles.weatherOtherSection}>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>降雨概率：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.prop}</Text>
+                  </View>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>湿度：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.humi}</Text>
+                  </View>
+                </View>
+                <View style={styles.weatherOtherSection}>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>风速：</Text>
+                    <Text style={styles.weatherOtherValue}><Text style={{fontSize: 10}}>{elem.dir}</Text>{elem.speed}</Text>
+                  </View>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>体感温度：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.feel}</Text>
+                  </View>
+                </View>
+                <View style={styles.weatherOtherSection}>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>降水量：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.rain}</Text>
+                  </View>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>气压：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.pres}</Text>
+                  </View>
+                </View>
+                <View style={styles.weatherOtherSection}>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>能见度：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.sight}</Text>
+                  </View>
+                  <View style={styles.weatherOtherLine}>
+                    <Text style={styles.weatherOtherTitle}>紫外线指数：</Text>
+                    <Text style={styles.weatherOtherValue}>{elem.uv}</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
         </View>
-        <View style={styles.container}>
-          <Text>Day 2</Text>
-        </View>
-        <View style={styles.container}>
-          <Text>Day 3</Text>
-        </View>
-      </Swiper>
+      )
+    })
+
+    return(
+      <View>
+        <Swiper
+          style={styles.wrapper}
+          showsButtons={false}
+          paginationStyle={{bottom: 10, paddingTop: 10, borderTopColor: 'rgba(255,255,255,0.7)', borderTopWidth: StyleSheet.hairlineWidth}}
+          dot={<View style={{backgroundColor: 'rgba(255,255,255,0.2)', width: 6, height: 6, borderRadius: 3, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+          activeDot={<View style={{backgroundColor: 'rgba(255,255,255,0.5)', width: 6, height: 6, borderRadius: 3, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}>
+          {slides}
+        </Swiper>
+        <TouchableHighlight onPress={() => this._back()} style={styles.backBtn}>
+          <Icon size={17} name='ios-list-outline' style={styles.backBtnIcon} />
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+export default class Day2 extends Component{
+  static propTypes = {
+    navigator: PropTypes.any.isRequired,
+  }
+
+  componentWillMount() {
+    StatusBar.setBarStyle('light-content')
+  }
+
+  _back() {
+    this.props.navigator.pop()
+  }
+
+  render() {
+    return(
+      <View style={styles.weatherContainer}>
+        <Weather back={() => this._back()} />
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
+  pageContainer: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    width: screenWidth,
+    left: 0,
+    top: 20,
+    height: screenHeight - 53,
   },
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
+  headInfo: {
+    paddingTop: 70,
     alignItems: 'center',
+    paddingBottom: 60,
+  },
+  city: {
+    fontSize: 25,
+    color: '#fff',
+    paddingBottom: 5,
+    backgroundColor: 'transparent',
+  },
+  abs: {
+    fontSize: 15,
+    color: '#fff',
+    backgroundColor: 'transparent',
+  },
+  degree: {
+    fontSize: 85,
+    color: '#fff',
+    fontWeight: '100',
+  },
+  circle: {
+    fontSize: 35,
+    color: '#fff',
+    fontWeight: '300',
+    position: 'absolute',
+    top: 130,
+    right: screenWidth / 2 - 55,
+  },
+  withinDayGeneral: {
+    flexDirection: 'row',
+    width: screenWidth,
+  },
+  withinDayHead: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingLeft: 20,
+  },
+  withinDayTail: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingRight: 10,
+  },
+  withinDayWeek: {
+    fontSize: 15,
+    color: '#fff',
+    fontWeight: '400',
+    width: 50,
+  },
+  withinDayDay: {
+    fontSize: 15,
+    color: '#fff',
+    fontWeight: '300',
+    width: 50,
+  },
+  withinDayHigh: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '200',
+    width: 30,
+  },
+  withinDayLow: {
+    fontSize: 16,
+    color: '#eee',
+    fontWeight: '200',
+    width: 30,
+  },
+  withinDayLowNight: {
+    fontSize: 16,
+    color: '#aaa',
+    fontWeight: '200',
+    width: 30,
+  },
+  withinDayHoursBox: {
+    width: 55,
+  },
+  withinDayHoursContainer: {
+    marginTop: 3,
+    borderTopColor: 'rgba(255,255,255,0.7)',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  withinDayHours: {
+    paddingLeft: 7,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 10,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+  withinDayHoursTime: {
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  withinDayHoursTimeBold: {
+    color: '#fff',
+    fontSize: 13,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  withinDayHoursIcon: {
+    textAlign: 'center',
+    paddingTop: 5,
+  },
+  withinDayHoursDegree: {
+    color: '#fff',
+    fontSize: 14,
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+  withinDayHoursDegreeBold: {
+    color: '#fff',
+    fontSize: 15,
+    textAlign: 'center',
+    paddingTop: 5,
+    fontWeight: '500',
+  },
+  withinWeek: {
+    paddingTop: 5,
+  },
+  withinWeekLine: {
+    flexDirection: 'row',
+    height: 28,
+  },
+  withinWeekDay: {
     justifyContent: 'center',
+    alignItems: 'flex-start',
+    flex: 1,
+  },
+  withinWeekIcon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  withinWeekDegree: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    paddingRight: 25,
+  },
+  withinWeekHigh: {
+    color: '#fff',
+    width: 35,
+    fontSize: 16,
+    textAlign: 'right',
+  },
+  withinWeekIconIcon: {
+    color: '#fff',
+  },
+  withinWeekLow: {
+    color: '#eee',
+    width: 35,
+    fontSize: 16,
+    textAlign: 'right',
+  },
+  withinWeekLowNight: {
+    color: '#aaa',
+    width: 35,
+    fontSize: 16,
+    textAlign: 'right',
+  },
+  withinWeekDayText: {
+    color: '#fff',
+    paddingLeft: 20,
+    fontSize: 15,
+  },
+  weatherInfo: {
+    marginTop: 5,
+    borderTopColor: 'rgba(255,255,255,0.7)',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  weatherInfoText: {
+    color: '#fff',
+    fontSize: 15,
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingBottom: 10,
+    paddingRight: 20,
+  },
+  weatherOther: {
+    paddingTop: 10,
+  },
+  weatherOtherSection: {
+    paddingBottom: 10,
+  },
+  weatherOtherLine: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+  },
+  weatherOtherTitle: {
+    width: screenWidth / 2 - 15,
+    color: '#fff',
+    textAlign: 'right',
+    fontSize: 15,
+  },
+  weatherOtherValue: {
+    width: screenWidth / 2,
+    paddingLeft: 15,
+    flex: 1,
+    fontSize: 15,
+    color: '#fff',
+  },
+  backBtn: {
+    position: 'absolute',
+    right: 20,
+    bottom: 7,
+  },
+  backBtnIcon: {
+    color: '#fff',
   },
 })
