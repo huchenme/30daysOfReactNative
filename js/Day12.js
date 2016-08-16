9d862a7f95a18e970b23b610edd6b95bc0ccab9c
Hu Chen <chen@huchen.me>
2016-08-16 23:58:41 +0800

    update for day12

--- js/Day12.js
+++ js/Day12.js
@@ -9,7 +9,7 @@ export default class Day12 extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Day 12</Text>
+        <Text>Day 12, won't do because the library is not stable</Text>
       </View>
     )
   }
